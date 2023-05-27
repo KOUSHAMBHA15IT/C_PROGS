c1c44a7ebee8cf4e59e3e9a7871ee892fbfb6a6a
KOUSHAMBHA15IT <122219149+KOUSHAMBHA15IT@users.noreply.github.com>
2023-05-27 09:48:23 +0530

    Create Special_char_print.c

---
+++ Special_char_print.c
@@ -0,0 +1,16 @@
+// print $ 
+// print %
+// print \
+// print ""
+// print '
+//print \n
+#include<stdio.h>
+void main()
+{
+	printf("$\n");
+	printf("%% \n");
+	printf("\\ \n");
+	printf("\"\"");
+	printf("\n ' \n");
+	printf("\\n");
+}
